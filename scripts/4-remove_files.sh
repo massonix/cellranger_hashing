e17775f98da0f422b2707accec90465451d6304d
massonix <ramon.massoni@cnag.crg.eu>
2019-11-04 12:27:32 +0100

    .gitignore

--- scripts/4-remove_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Removes the temporary files of cellranger's output
-project_path=$(pwd)
-jobs_path="${project_path}/jobs"
-cd "$jobs_path"
-for dir in ./*; do
-    rm -r "${dir}/${dir}/SC_RNA_COUNTER_CS"
-done
