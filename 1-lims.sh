0602ea15345d434744cdbe2ddfcbc2cd6998566f
massonix <ramon.massoni@cnag.crg.eu>
2020-01-09 10:47:59 +0100

    Change # to _ in info.txt

--- 1-lims.sh
+++ 1-lims.sh
@@ -6,4 +6,4 @@ module load lims/1.2
 
 # Get information for each library (flow cell, lane, sample id, etc.)
 # $1  needs to be the name of the project
-/project/production/DAT/apps/LIMSQ/limsq -sp $1 | sed 's/;/\t/g' > info.txt
+/project/production/DAT/apps/LIMSQ/limsq -sp $1 | sed 's/;/\t/g' | sed 's/#/_/g' > info.txt
